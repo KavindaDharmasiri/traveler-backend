216ab05aaa187849fc0e85d8a775a74dabd76681
KavindaGimhan <kavinda_d@epiclanka.net>
2025-12-02 16:25:35 +0530

    item crud done

--- traveler-core/src/main/java/com/traveler/core/repository/ItemRepository.java
+++ traveler-core/src/main/java/com/traveler/core/repository/ItemRepository.java
@@ -2,9 +2,15 @@ package com.traveler.core.repository;
 
 import com.traveler.common.entity.Trip;
 import com.traveler.common.entity.provider.Item;
+import com.traveler.common.utils.STATUS;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
+    List<Item> findAllByStatus(STATUS status);
 }
