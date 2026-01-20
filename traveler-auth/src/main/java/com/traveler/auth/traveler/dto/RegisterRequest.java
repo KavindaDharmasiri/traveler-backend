bc15f72ffc651281f3d645450f22fda015fc25c4
KavindaGimhan <kavinda_d@epiclanka.net>
2026-01-20 16:34:11 +0530

    add twilio

--- traveler-auth/src/main/java/com/traveler/auth/traveler/dto/RegisterRequest.java
+++ traveler-auth/src/main/java/com/traveler/auth/traveler/dto/RegisterRequest.java
@@ -40,8 +40,6 @@ public class RegisterRequest {
     private String nicImageUuid;
     private String nicBackUuid;
     @NotBlank
-    @Pattern(regexp = "^https://maps\\.google\\.com/.*|^https://www\\.google\\.com/maps/.*|^https://goo\\.gl/maps/.*$", 
-             message = "Please provide a valid Google Maps link")
     private String googleMapsUrl;
 
     @NotNull
